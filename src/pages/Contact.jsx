f0f76653fcf7a0243dc9697681309ca7bf1d67a9
lukevonbergen <luke_vb1@outlook.com>
2026-01-01 16:40:49 +0000

    maps

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -200,7 +200,7 @@ const Contact = () => {
 
           <div className="map-container reveal">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2475.8!2d-0.7!3d51.58!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTHCsDM0JzQ4LjAiTiAwwrA0MicwMC4wIlc!5e0!3m2!1sen!2suk!4v1234567890"
+              src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Dolce%20Vita%20Wooburn&t=&z=15&ie=UTF8&iwloc=B&output=embed"
               width="100%"
               height="400"
               style={{ border: 0 }}
